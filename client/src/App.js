be774b65133ede69b550e21d007af4db5ff24e5c
Louis Klein <louisklein71@gmail.com>
2023-10-19 02:02:07 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 
+// components
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 function App() {
   return (
     <div className="App">
+      <Header/>
       Unga Bunga
+      <Footer/>
     </div>
+    
   );
 }
 
