781829e77f176aab23496c0c985c8c1110ce9f80
Louis Klein <louisklein71@gmail.com>
2023-10-19 02:17:12 -0700

    Update navbar.js

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { NavLink, useNavigate } from 'react-router-dom';
 
 const navbar = () => {
   return (
-    <div>navbar</div>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About Me</NavLink>
+        <NavLink to="/resume" target="_blank" rel="noopener noreferrer">Resume</NavLink>
+        <NavLink to="/projects">Projects</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+      </nav>
   )
 }
 
