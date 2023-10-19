7cca8e2a3c3b4c71b6c9c70fb79e170f771e3eb4
Louis Klein <louisklein71@gmail.com>
2023-10-19 02:03:29 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
+// imports
 import './App.css';
+import React, {createContext, useState, useEffect} from 'react';
+import {Routes, Route, useNavigate} from 'react-router-dom';
 
 // components
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Home from './components/Home';
+import Links from './components/Links';
+import Navbar from './components/Navbar';
+import Projects from './components/Projects';
+import Summary from './components/Summary';
 
 function App() {
   return (
