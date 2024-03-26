eeafa273c1e39b4a1ac93bd41c5746ba2a79c5e3
Louis Klein <louisklein71@gmail.com>
2024-03-26 03:08:05 -0700

    new resume

--- client/src/components/Resume.js
+++ client/src/components/Resume.js
@@ -1,17 +1,9 @@
-import React, { useEffect } from 'react';
-import MyResume from "../Resume.pdf";
+import React from 'react'
 
 const Resume = () => {
-
-  // later attach this to "about" page
   return (
-    <div>
-        <a href={MyResume} target="_blank"
-            rel="noreferrer">
-            Resume
-        </a> 
-    </div>
-  );
+    <div>Resume</div>
+  )
 }
 
-export default Resume;
+export default Resume
---
+++ client/src/components/ResumeFile.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+import MyResume from "../Resume.pdf";
+
+const Resume = () => {
+  // later attach this to "about" page
+  return (
+    <div>
+        <a href={MyResume} target="_blank"
+            rel="noreferrer">
+            Resume
+        </a> 
+    </div>
+  );
+}
+
+export default Resume;
