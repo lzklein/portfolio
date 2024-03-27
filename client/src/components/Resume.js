efe34f090922f11182cc83d3f980823bc2e954ba
Louis Klein <louisklein71@gmail.com>
2024-03-26 22:06:28 -0700

    Update Resume.js

--- client/src/components/Resume.js
+++ client/src/components/Resume.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import ResumeFile from './ResumeFile';
 
 const Resume = () => {
   return (
-    <div>Resume</div>
+    <div>
+        <h1>Resume</h1>
+        <ResumeFile/>
+    </div>
   )
 }
 
