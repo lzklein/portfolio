c27bd1cf8b8fd74d902df2882ec6d412c40e7fa4
lzklein <louisklein71@gmail.com>
2024-04-07 19:24:03 -0700

    moved navbar

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import Navbar from './Navbar'
 import  Game from '../games/Game.js';
 
 const home = () => {
   // navbar on left vertical => update with scroll?
   return (
     <div>
-      <Navbar/>
       Home
       <Game/>
     </div>
