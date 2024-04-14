c4b2ec80b895a3cc1c35a12a520b92597d1a0206
Louis Klein <louisklein71@gmail.com>
2024-04-13 21:48:19 -0700

    Update Projects.js

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,12 +1,17 @@
 import React from 'react'
 
 // maybe add arcade easter egg here
+// have side nav thing to jump around the page with project name, https://brittanychiang.com/ like this thing
 const projects = () => {
   return (
     <div>
       <h1>Projects</h1>
-      <p>SmokePlus</p>
-      <p>WangusBot</p>
+      <h2>SmokePlus</h2>
+      <a href="https://smokeplus.onrender.com/" target="_blank">https://smokeplus.onrender.com/</a>
+      <p>description goes here</p>
+      <h2>WangusBot</h2>
+      <p>maybe an image here?</p>
+      <p>description goes here</p>
     </div>
   )
 }
