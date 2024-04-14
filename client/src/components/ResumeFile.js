d9fc9b6d5ec75ee8951f119938582231e35a680b
Louis Klein <louisklein71@gmail.com>
2024-04-13 21:53:18 -0700

    Update ResumeFile.js

--- client/src/components/ResumeFile.js
+++ client/src/components/ResumeFile.js
@@ -5,6 +5,8 @@ const Resume = () => {
   // later attach this to "about" page
   return (
     <div>
+        <embed src={MyResume} type="application/pdf" width="100%" height="600px" />
+
         <a href={MyResume} target="_blank"
             rel="noreferrer">
             Resume
