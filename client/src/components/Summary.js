36cf6021c1366e2e0b4592c193d877a3e3bc6224
Louis Klein <louisklein71@gmail.com>
2024-04-15 18:18:48 -0700

    Update Summary.js

--- client/src/components/Summary.js
+++ client/src/components/Summary.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Links from './Links.js';
 
 // maybe don't need / put this in home page
 const Summary = () => {
   return (
-    <div>About me</div>
+    <div>
+      <h1>About Me</h1>
+      <p>Blurb</p>
+      <Links/>
+    </div>
   )
 }
 
