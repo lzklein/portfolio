eb1df6cff34a6a3fbda05aba612838e1fab3f6eb
Louis Klein <louisklein71@gmail.com>
2024-06-08 14:28:07 -0700

    no phone

--- client/src/components/Links.js
+++ client/src/components/Links.js
@@ -6,7 +6,6 @@ const Links = () => {
       <a href="https://www.linkedin.com/in/louis-klein-audio/" target="_blank">LinkedIn</a>
       <a href="https://github.com/lzklein" target="_blank">GitHub</a>
       <a href="mailto:louisklein71@gmail.com" target="_blank">Send Email</a>
-      <p>Phone</p>
     </div>
   )
 }
