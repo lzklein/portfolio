0b670f89279b92072b14b9205eefe1723c1bac2e
Louis Klein <louisklein71@gmail.com>
2024-06-25 01:52:32 -0700

    Update Technologies.js

--- client/src/components/Technologies.js
+++ client/src/components/Technologies.js
@@ -4,7 +4,14 @@ const Technologies = () => {
     // list out technologies (languages) can use?
     // click to show gif/screenshot or something example of technology i did?
   return (
-    <div>Technologies</div>
+    <div>
+    <h2>Technologies</h2>
+    <ul>
+      <li>
+        React
+      </li>
+    </ul>
+    </div>
   )
 }
 
