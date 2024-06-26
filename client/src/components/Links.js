784a1174daf028909625ef31f5ad409134418a56
Louis Klein <louisklein71@gmail.com>
2024-06-26 15:51:17 -0700

    Update Links.js

--- client/src/components/Links.js
+++ client/src/components/Links.js
@@ -1,30 +1,12 @@
 import React from 'react'
 
-// favicon icons
-import linkedinIcon from '../favicons/linkedin.png';
-import githubIcon from '../favicons/github.png';
-import emailIcon from '../favicons/email.png';
-
 const Links = () => {
-  // maybe favicon images instead of text links
   return (
     <div>
-      <a href="https://www.linkedin.com/in/louis-klein-audio/" target="_blank" rel="noreferrer">
-        <img src={linkedinIcon} alt="LinkedIn" className='favicon'/>
-      </a>
-      <a href="https://github.com/lzklein" target="_blank" rel="noreferrer">
-        <img src={githubIcon} alt="GitHub"  className='favicon'/>
-      </a>
-      <a href="mailto:louisklein71@gmail.com" target="_blank" rel="noreferrer">
-        <img src={emailIcon} alt="Email"  className='favicon'/>
-      </a>
+    <a href="https://www.linkedin.com/in/louis-klein-audio/" target="_blank" rel="noreferrer" className='contact-link'>LinkedIn</a>
+    <a href="https://github.com/lzklein" target="_blank" rel="noreferrer" className='contact-link'>GitHub</a>
+    <a href="mailto:louisklein71@gmail.com" target="_blank" rel="noreferrer" className='contact-link'>Send Email</a>
     </div>
-
-    // <div>
-    // <a href="https://www.linkedin.com/in/louis-klein-audio/" target="_blank" rel="noreferrer" className='contact-link'>LinkedIn</a>
-    // <a href="https://github.com/lzklein" target="_blank" rel="noreferrer" className='contact-link'>GitHub</a>
-    // <a href="mailto:louisklein71@gmail.com" target="_blank" rel="noreferrer" className='contact-link'>Send Email</a>
-    // </div>
   )
 }
 
