d66f0fbef34bc3d6842bb793707230ab189c2bbc
Louis Klein <louisklein71@gmail.com>
2024-06-28 00:06:31 -0700

    icons in footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Favicons from './Favicons';
 
 const Footer = () => {
   return (
     <div>
       <p>copyright? more links?</p>
+      <Favicons/>
       {/* credits, maybe links via icons https://brittanychiang.com */}
     </div>
   )
