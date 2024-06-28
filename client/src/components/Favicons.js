51c80614d2c927610cf48c93573409ca1ab5424b
Louis Klein <louisklein71@gmail.com>
2024-06-28 00:06:22 -0700

    class names

--- client/src/components/Favicons.js
+++ client/src/components/Favicons.js
@@ -8,13 +8,13 @@ import emailIcon from '../favicons/email.png';
 const Favicons = () => {
   return (
     <div>
-      <a href="https://www.linkedin.com/in/louis-klein-audio/" target="_blank" rel="noreferrer">
+      <a href="https://www.linkedin.com/in/louis-klein-audio/" target="_blank" rel="noreferrer" className='icon-container'>
         <img src={linkedinIcon} alt="LinkedIn" className='favicon'/>
       </a>
-      <a href="https://github.com/lzklein" target="_blank" rel="noreferrer">
+      <a href="https://github.com/lzklein" target="_blank" rel="noreferrer" className='icon-container'>
         <img src={githubIcon} alt="GitHub"  className='favicon'/>
       </a>
-      <a href="mailto:louisklein71@gmail.com" target="_blank" rel="noreferrer">
+      <a href="mailto:louisklein71@gmail.com" target="_blank" rel="noreferrer" className='icon-container'>
         <img src={emailIcon} alt="Email"  className='favicon'/>
       </a>
     </div>
