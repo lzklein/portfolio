ff4b23da356e3a4131a5e5be969a10b183a234af
Louis Klein <louisklein71@gmail.com>
2024-07-04 04:46:24 -0700

    old note remove

--- client/src/components/ResumeFile.js
+++ client/src/components/ResumeFile.js
@@ -2,7 +2,6 @@ import React from 'react';
 import MyResume from "../Resume.pdf";
 
 const Resume = () => {
-  // later attach this to "about" page
   return (
     <div>
         <embed src={MyResume} type="application/pdf" width="75%" height="1000px" />
