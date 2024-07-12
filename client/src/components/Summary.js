1c2a0f9aaf1884ad5b95087e7a38a2864af1889e
Louis Klein <louisklein71@gmail.com>
2024-07-12 00:55:01 -0700

    Update Summary.js

--- client/src/components/Summary.js
+++ client/src/components/Summary.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Links from './Links.js';
 
 // maybe don't need / put this in home page
 const Summary = () => {
@@ -8,8 +7,8 @@ const Summary = () => {
       {/* use intro transition with h1 fade to p? */}
       <h1>About Me</h1>
       {/* transitions while scrolling down, maybe changing bg images */}
+      {/* font size change for focus? */}
       <p>Blurb</p>
-      <Links/>
     </div>
   )
 }
