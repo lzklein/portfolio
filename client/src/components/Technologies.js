1e57a162b1c2be0f9d1eaa347885625f08f098ee
Louis Klein <louisklein71@gmail.com>
2024-07-14 18:29:15 -0700

    Update Technologies.js

--- client/src/components/Technologies.js
+++ client/src/components/Technologies.js
@@ -1,11 +1,10 @@
 import React from 'react'
 
 const Technologies = () => {
-    // list out technologies (languages) can use?
     // click to show gif/screenshot or something example of technology i did?
   return (
     <div>
-    <h2>Technologies</h2>
+    <h3>Technologies</h3>
     <ul>
       <li>
         React
