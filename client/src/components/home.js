25b04b2ca9eb93633f53c20da13c5dca6a900d02
Louis Klein <louisklein71@gmail.com>
2025-09-06 01:05:03 -0700

    rework

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Arcade from './Arcade.js';
 
 // TODO
 // !primary -- Content, basic appearances
@@ -12,14 +11,8 @@ const Home = () => {
     <div>
       <h1>Louis Klein</h1>
       <h3>Software Engineer</h3>
-      {/* make website "3d" */}
-      {/* maybe have toggle between true 3d and 3d glasses 3d red/blue blurry */}
-      {/* 3d via layers different heights */}
-      {/* nav button tilt page angle view */}
-      {/* https://cobblemon.com/en */}
-
-      {/* some sort of intro transition? */}
-      {/* WELCOME => header footer slide in => welcome transitions to HOME  */}
+      {/* td game */}
+      {/* scavenger hunt towers */}
     </div>
   )
 }
