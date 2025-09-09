a2c87bfad35771317120b0fb7b16f448807f83f2
Louis Klein <louisklein71@gmail.com>
2025-09-08 21:07:05 -0700

    game board

--- client/src/components/td/Game.js
+++ client/src/components/td/Game.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useRef, useEffect } from "react";
+import boardImg from "./assets/sprites/board.png";
 
-const Game = () => {
+export default function Game() {
+  const canvasRef = useRef(null);
+  const tileSize = 32; //? 32x32 tilesize, 17x11 board, 544x352px total
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+
+    // Load the board background
+    const board = new Image();
+    board.src = boardImg;
+    board.onload = () => {
+      ctx.imageSmoothingEnabled = false; // keep pixel look
+      ctx.drawImage(board, 0, 0, canvas.width, canvas.height);
+    };
+  }, []);
+
   return (
-    <div>Game</div>
-  )
+    <canvas
+      ref={canvasRef}
+      width={1088} // 17 * 32 * 2
+      height={704} // 11 * 32 * 2
+      style={{
+        border: "2px solid black",
+        imageRendering: "pixelated",
+      }}
+    />
+  );
 }
-
-export default Game
