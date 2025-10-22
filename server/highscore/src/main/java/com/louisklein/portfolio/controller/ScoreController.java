b8641c1e1f953086d268a31a6b8a3c1d1be980c3
Louis Klein <louisklein71@gmail.com>
2025-10-22 14:23:50 -0700

    rest setup

--- server/highscore/src/main/java/com/louisklein/portfolio/controller/ScoreController.java
+++ server/highscore/src/main/java/com/louisklein/portfolio/controller/ScoreController.java
@@ -1,7 +1,14 @@
 package com.louisklein.portfolio.controller;
 
+import com.louisklein.portfolio.model.Score;
 import com.louisklein.portfolio.service.ScoreService;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/scores")
+@CrossOrigin(origins = "*")
 public class ScoreController {
     private final ScoreService service;
 
@@ -9,4 +16,13 @@ public class ScoreController {
         this.service = service;
     }
 
+    @GetMapping
+    public List<Score> getTopScores() {
+        return service.getTopScores();
+    }
+
+    @PostMapping
+    public void addScore(@RequestBody Score score) {
+        service.addScore(score.getScore(), score.getInitials());
+    }
 }
