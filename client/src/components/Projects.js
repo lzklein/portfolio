3cab6f63d1542da1b8c6c9d365cfe9611614580b
Louis Klein <louisklein71@gmail.com>
2026-01-13 00:49:00 -0800

    project page

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -8,41 +8,41 @@ import PlaylistCrusader from '../images/PlaylistCrusader.png';
 // 3d effect, put projects in cards and hover moves card forward?
 const projects = () => {
 
-  return (
-    <div>
+return (
+    <div className="projects-page">
       <h1>Projects</h1>
-      <div className='project-card'>
-        <h2>Playlist Crusader</h2>
-        <img src={PlaylistCrusader} style={{maxWidth:"60%"}}/>
-        <h4>ReactJS - Java</h4>
-        <p>Music Playlist Social Platform</p>
-        <p>Fully customizeable playlists to share with 'like' functionality</p>
-      </div>
-      <div className='project-card'>
-        <h2>SmokePlus Online Storefront</h2>
-        <a href="https://smokeplus.onrender.com/" target="_blank" rel="noreferrer">
-          <img src={Smokeplus} style={{width:'800px'}}/>
-        </a>
-        <h4>ReactJS - ExpressJS</h4>
-        <p>Set up pickup orders</p>
-        <p>Manage inventory as an employee</p>
 
-        {/* 3 bullet points of site function, technology used */}
-        {/* maybe technology used as separate thing */}
-        {/* hard code or auto components? */}
-        
-      </div>
-      <div  className='project-card'>
-        <h2>WangusBot</h2>
-        <img src={DiscordBot}/> 
-        <br/>
-        <img src={DiscordBot2} style={{maxWidth:"50%"}}/>
-        <h4>DiscordJS</h4>
-        <p>Discord Music Bot</p>
-        <p>Downloads and plays audio via Youtube link</p>
+      <div className="projects-carousel">
+        <div className="project-card">
+          <h2>Playlist Crusader</h2>
+          <img src={PlaylistCrusader} style={{ maxWidth: "60%" }} />
+          <h4>ReactJS - Java</h4>
+          <p>Music Playlist Social Platform</p>
+          <p>Fully customizable playlists with like functionality</p>
+        </div>
+
+        <div className="project-card">
+          <h2>SmokePlus Online Storefront</h2>
+          <a href="https://smokeplus.onrender.com/" target="_blank" rel="noreferrer">
+            <img src={Smokeplus} style={{ width: "800px" }} />
+          </a>
+          <h4>ReactJS - ExpressJS</h4>
+          <p>Set up pickup orders</p>
+          <p>Manage inventory as an employee</p>
+        </div>
+
+        <div className="project-card">
+          <h2>WangusBot</h2>
+          <img src={DiscordBot} />
+          <br />
+          <img src={DiscordBot2} style={{ maxWidth: "50%" }} />
+          <h4>DiscordJS</h4>
+          <p>Discord Music Bot</p>
+          <p>Downloads and plays audio via YouTube link</p>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default projects
