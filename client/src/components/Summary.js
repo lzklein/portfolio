6b75baa97b18878b7bd31757fc25f50689cb38b2
Louis Klein <louisklein71@gmail.com>
2026-03-11 02:32:10 -0700

    more fleshed out page

--- client/src/components/Summary.js
+++ client/src/components/Summary.js
@@ -1,14 +1,31 @@
 import React from 'react'
 
-// maybe don't need / put this in home page
 const Summary = () => {
   return (
     <div>
-      {/* use intro transition with h1 fade to p? */}
+
       <h2>About Me</h2>
-      {/* transitions while scrolling down, maybe changing bg images */}
-      {/* font size change for focus? */}
-      <p>Blurb</p>
+
+      <p>
+        Blurb about myself. I'll write it eventually.
+      </p>
+
+      <h3>Technologies</h3>
+      <ul>
+        <li>React</li>
+        <li>Java / Spring Boot</li>
+        <li>JavaScript</li>
+        <li>Python</li>
+        <li>Git</li>
+      </ul>
+
+      <h3>Current/Planned projects</h3>
+      <ul>
+        <li>Manga translation tool (OpenCV)</li>
+        <li>Whiteboarding tool (Live Multi-User)</li>
+        <li>Finishing this</li>
+      </ul>
+
     </div>
   )
 }
