d4736757b1ea99c02415b51bb3e1c5306bc7047c
Louis Klein <louisklein71@gmail.com>
2026-03-12 03:40:43 -0700

    refactored cards

--- client/src/components/ProjectCard.js
+++ client/src/components/ProjectCard.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
-const ProjectCard = () => {
+const ProjectCard = ({ title, image, tech, description, extra, link }) => {
   return (
-    <div>ProjectCard</div>
-  )
+    <div className="project-card">
+      <h2>{title}</h2>
+
+      {link ? (
+        <a href={link} target="_blank" rel="noreferrer">
+          <img src={image} style={{ maxWidth: "90%" }} />
+        </a>
+      ) : (
+        <img src={image} style={{ maxWidth: "90%" }} />
+      )}
+
+      <h4>{tech}</h4>
+      <p>{description}</p>
+      {extra && <p>{extra}</p>}
+    </div>
+  );
 }
 
 export default ProjectCard
