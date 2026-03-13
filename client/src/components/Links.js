a4fb5fcdc042c6e15b44b36b50306c92b9430906
Louis Klein <louisklein71@gmail.com>
2026-03-13 14:56:36 -0700

    navbar adjustment

--- client/src/components/Links.js
+++ client/src/components/Links.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Links = () => {
   return (
-    <div>
+    <div className="contact-links">
       <a href="https://www.linkedin.com/in/louis-klein-audio/" target="_blank" rel="noreferrer" className='contact-link'>LinkedIn</a>
       <a href="https://github.com/lzklein" target="_blank" rel="noreferrer" className='contact-link'>GitHub</a>
       <a href="mailto:louisklein71@gmail.com" target="_blank" rel="noreferrer" className='contact-link'>Email</a>
