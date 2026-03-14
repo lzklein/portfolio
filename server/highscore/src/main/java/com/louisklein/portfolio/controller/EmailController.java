62bd719d956a58c205d57b2d722dfa72b2067850
Louis Klein <louisklein71@gmail.com>
2026-03-14 08:37:37 -0700

    Create EmailController.java

---
+++ server/highscore/src/main/java/com/louisklein/portfolio/controller/EmailController.java
@@ -0,0 +1,10 @@
+package com.louisklein.portfolio.controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/scores")
+@CrossOrigin(origins = "*")
+public class EmailController {
+}
