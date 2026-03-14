d29486f969c1df85c9edd31d1ca54bd0c92e3b00
Louis Klein <louisklein71@gmail.com>
2026-03-14 08:47:50 -0700

    submission handling

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react'
+import React, {useState, useRef} from 'react'
+import ReCAPTCHA from "react-google-recaptcha";
 import Links from './Links.js'
 
 const Contact = () => {
@@ -6,6 +7,33 @@ const Contact = () => {
   const [subject, setSubject] = useState("");
   const [message, setMessage] = useState("");
   const [obviousBotTrap, setObviousBotTrap] = useState("");
+  const [captchaVerified, setCaptchaVerified] = useState(false);
+
+  const recaptchaRef = useRef(null);
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+
+      if(!email){
+        alert("Please provide a return email");
+        return;
+      }
+
+      if (!captchaVerified) {
+        alert("Please verify the captcha before submitting.");
+        return;
+      }
+
+      // TODO: send email to backend
+      console.log({ email, subject, message, obviousBotTrap });
+
+      setEmail("");
+      setSubject("");
+      setMessage("");
+      setObviousBotTrap("");
+      recaptchaRef.current.reset();
+      setCaptchaVerified(false);
+    };
 
   return (
     <div className="page">
@@ -15,11 +43,17 @@ const Contact = () => {
         <Links/>
         <p className="contact-note">Socials are also available in the footer.</p>
 
-        <form className="contact-form">
-          <input type="text" placeholder="Email" />
-          <input type="text" placeholder="Subject" />
-          <textarea placeholder="Message" rows="5"></textarea>
-          <input type="text" name="company" style="display:none" />
+        <form className="contact-form" onSubmit={handleSubmit}>
+          <input type="text" placeholder="Email" onChange={(e)=>{setEmail(e.target.value)}} value={email}/>
+          <input type="text" placeholder="Subject" onChange={(e)=>{setSubject(e.target.value)}} value={subject}/>
+          <textarea placeholder="Message" rows="5" onChange={(e)=>{setMessage(e.target.value)}} value={message}></textarea>
+          <input type="text" name="company" style={{display:"none"}} onChange={(e)=>{setObviousBotTrap(e.target.value)}} value={obviousBotTrap}/>
+
+          <ReCAPTCHA
+            sitekey="6LfmtoksAAAAAIrnAN9y5qb_I88CsWbFK33ZixAW"
+            onChange={() => setCaptchaVerified(true)}
+            ref={recaptchaRef}
+          />
 
           <button type="submit">Send Email</button>
         </form>
