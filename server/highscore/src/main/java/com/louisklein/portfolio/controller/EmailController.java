339f5cece293cc622a0c686872f3d7225e32f64e
Louis Klein <louisklein71@gmail.com>
2026-03-15 00:42:37 -0700

    building out email handler

--- server/highscore/src/main/java/com/louisklein/portfolio/controller/EmailController.java
+++ server/highscore/src/main/java/com/louisklein/portfolio/controller/EmailController.java
@@ -1,10 +1,37 @@
 package com.louisklein.portfolio.controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.louisklein.portfolio.service.EmailService;
+import com.louisklein.portfolio.service.RateLimitService;
+import io.github.bucket4j.Bucket;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/scores")
+@RequestMapping("/api/email")
 @CrossOrigin(origins = "*")
 public class EmailController {
+    @Autowired
+    private RateLimitService rateLimitService;
+
+    @Autowired
+    private EmailService emailService;
+
+    @PostMapping
+    public <ContactForm> ResponseEntity<?> submitForm(
+            @RequestBody ContactForm form,
+            HttpServletRequest request) {
+
+        String ip = request.getRemoteAddr();
+        Bucket bucket = rateLimitService.resolveBucket(ip);
+
+        if (!bucket.tryConsume(1)) {
+            return ResponseEntity
+                    .status(HttpStatus.TOO_MANY_REQUESTS)
+                    .body("Rate limit exceeded. Try again later.");
+        }
+
+        return ResponseEntity.ok("Message sent");
+    }
 }
