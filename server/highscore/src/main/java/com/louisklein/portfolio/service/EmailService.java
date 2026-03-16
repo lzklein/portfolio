779956a7bac9635a9e095f2227874b04523ab86e
Louis Klein <louisklein71@gmail.com>
2026-03-16 00:03:18 -0700

    refactored

--- server/highscore/src/main/java/com/louisklein/portfolio/service/EmailService.java
+++ server/highscore/src/main/java/com/louisklein/portfolio/service/EmailService.java
@@ -1,6 +1,8 @@
 package com.louisklein.portfolio.service;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -11,9 +13,16 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Value("${spring.mail.username}")
+    private String contactEmail;
+
     public void sendContactEmail(String email, String subject, String message) {
+
+        // Prevent header injection
+        subject = subject.replaceAll("[\\r\\n]", "");
+
         SimpleMailMessage mail = new SimpleMailMessage();
-        mail.setTo("your_email@gmail.com");
+        mail.setTo(contactEmail);
         mail.setSubject("Portfolio Contact: " + subject);
         mail.setText("From: " + email + "\n\n" + message);
 
