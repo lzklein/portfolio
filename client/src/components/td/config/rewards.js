7afa9ba2ad0e6b993da43cc4e759f64a72b4932a
Louis Klein <louisklein71@gmail.com>
2026-03-19 03:32:38 -0700

    Create rewards.js

---
+++ client/src/components/td/config/rewards.js
@@ -0,0 +1,11 @@
+export const REWARDS = [
+  ['acid',1],
+  ['slow', 1],
+  ['cannon',1],
+  ['arrow',2],
+  ['sniper',1],
+  ['buff',2],
+  ['chain',1],
+]
+
+export const RARE_REWARDS = ['map','arrow','cannon','slow','sniper','buff','chain','acid']
