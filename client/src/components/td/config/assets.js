eb36ba803eaeb212f45c47f19bc9385037a176e6
Louis Klein <louisklein71@gmail.com>
2026-03-19 03:32:27 -0700

    Create assets.js

---
+++ client/src/components/td/config/assets.js
@@ -0,0 +1,32 @@
+import impSheet from "../assets/sprites/imp-sheet.png";
+import eliteSheet from "../assets/sprites/elite-sheet.png";
+import fastSheet from "../assets/sprites/fast-sheet.png";
+import splitterSheet from "../assets/sprites/splitter-sheet.png";
+import flyerSheet from "../assets/sprites/flyer-sheet.png";
+import bossSheet from "../assets/sprites/boss-sheet.png";
+
+import wallImgSrc from "../assets/sprites/wall.png";
+import arrowImgSrc from "../assets/sprites/arrow.png";
+import cannonImgSrc from "../assets/sprites/cannon.png";
+import slowImgSrc from "../assets/sprites/slow.png";
+import acidImgSrc from "../assets/sprites/acid.png";
+import chainImgSrc from "../assets/sprites/chain.png";
+import sniperImgSrc from "../assets/sprites/sniper.png";
+import buffImgSrc from "../assets/sprites/buff.png";
+
+export const SPRITE_PATHS = {
+  imp: impSheet,
+  elite: eliteSheet,
+  fast: fastSheet,
+  splitter: splitterSheet,
+  flyer: flyerSheet,
+  boss: bossSheet,
+  wall: wallImgSrc,
+  arrow: arrowImgSrc,
+  cannon: cannonImgSrc,
+  slow: slowImgSrc,
+  acid: acidImgSrc,
+  chain: chainImgSrc,
+  sniper: sniperImgSrc,
+  buff: buffImgSrc,
+};
