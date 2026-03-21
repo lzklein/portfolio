b99d2cfb7fb2489cab2dfb78622437b29ffaacdb
Louis Klein <louisklein71@gmail.com>
2026-03-21 00:43:39 -0700

    Create waves.js

---
+++ client/src/components/td/config/waves.js
@@ -0,0 +1,18 @@
+export const WAVE_TEMPLATES = [
+  ['imp', 'imp', 'imp'],
+  ['imp', 'elite'],
+  ['fast', 'fast'],
+  ['splitter'],
+  ['flyer', 'flyer'],
+  ['elite', 'flyer'],
+];
+
+export const CHALLENGE_WAVE_TEMPLATES = [
+  ['elite', 'elite'],
+  ['imp','imp','imp','imp','imp'],
+  ['flyer','flyer','flyer'],
+  ['boss'],
+  ['splitter','imp','splitter'],
+  ['fast','splitter','fast'],
+  ['elite','splitter','fast']
+]
