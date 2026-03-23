3e4d79e2bd39aa2b71a5472b8309724c5345f09f
Louis Klein <louisklein71@gmail.com>
2026-03-23 15:32:56 -0700

    Update GameContainer.js

--- client/src/components/td/GameContainer.js
+++ client/src/components/td/GameContainer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import GameCanvas from "./GameCanvas";
 import ControlsPanel from "./ControlsPanel";
 import BonusModal from "./BonusModal";
@@ -10,8 +10,35 @@ export default function GameContainer(props) {
     showBonusModal,
     placeWallMode,
   } = props;
+  const [loading, setLoading] = useState(true);
+  const [click, setClick] = useState(0);
+  useEffect(()=>{
+    if(click>2){
+      setLoading(false);
+    }
+  },[click])
 
+  if (loading) {
+    return (
+      <div       style={{
+        textAlign: "center",
+        padding: "100px 200px 100px 200px",
+        marginBottom: "60px",
+        border: "2px solid #ffffff",
+        borderRadius: "12px",
+        backgroundColor: "rgba(255, 255, 255, 0.05)", // subtle glass look
+        maxWidth: "500px",
+        marginLeft: "auto",
+        marginRight: "auto",
+        boxShadow: "0 0 20px rgba(100,150,255,0.15)",
+      }} onClick={()=>setClick(click+1)}>
+        <h3 style={{marginBottom:"40px"}}>🚧 Feature Under Construction 🚧</h3>
+        <p>This area is still being built.</p>
+      </div>
+    );
+  }
   return (
+    
     <div style={{ textAlign: "center", position: "relative", display: "inline-block" }}>
       
       <ControlsPanel {...props} />
