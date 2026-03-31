e664bb4318049339a7b2f7d1ee74ac91f2c4c92b
Louis Klein <louisklein71@gmail.com>
2026-03-31 01:26:08 -0700

    mobile download

--- client/src/components/Resume.js
+++ client/src/components/Resume.js
@@ -1,7 +1,27 @@
 import React from 'react'
 import MyResume from "../assets/files/Resume.pdf";
 import JavaCert from '../assets/files/JavaCert.pdf'
+import { useIsMobile } from '../hooks/useIsMobile';
+
 const Resume = () => {
+  const isMobile = useIsMobile();
+
+  if (isMobile) {
+  return (
+    <div className="page">
+      <p>Download or view my resume:</p>
+      <a 
+        href={MyResume} 
+        target="_blank" 
+        rel="noopener noreferrer"
+        className="download-link"
+      >
+        Open Resume
+      </a>
+    </div>
+  );
+}
+
   return (
     <div className="page">
         <h2>Resume</h2>
